db6307dee6ff19ed07f9ff1f513bb12d26e0fa61
imankamrann <imankamran@gmail.com>
2024-05-17 14:37:45 -0400

    lots of css for nav bar, header and styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, {useState} from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css'
-import logo from '../../assets/Baymax.webp';
+import logo from '../../assets/spaceCat.png';
+
+
+const Menu = () => (
+    // react fragement tags are wrapped around links
+    <>
+        <p><a href='#home'>Home</a></p>
+            <p><a href='#about'>About</a></p>
+            <p><a href='#photos'>Photos</a></p>
+            <p><a href='#art'>Art</a></p>
+        <p><a href='#more'>More</a></p>
+    </>
+)
 
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false)
+
   return (
       <div className='blog__navbar'>
           <div className='blog__navbar-links'>
               <div className='blog__navbar-links_logo'>
-                  <img src={logo} alt='logo' />
+                  <a href="#home">
+            <img src={logo} alt='logo' />
+            </a>
               </div>
               <div className='blog__navbar-links_container'>
-                  <p><a href='#home'>Home</a></p>
-                  <p><a href='#about'>About</a></p>
-                  <p><a href='#photos'>Photos</a></p>
-                  <p><a href='#art'>Art</a></p>
-                  <p><a href='#more'>More</a></p>
+                  <Menu/>
 
               </div>
+          </div>
+
+          <div className='blog__navbar-menu'>
+             {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+                  : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+              
+              {toggleMenu && (
+                  <div className='blog__navbar-menu_container scale-up-center'>
+                      <div className='blog__navbar-menu_container-links'>
+                          <Menu />
+                      </div>
+                  </div>
+              ) }
           </div>
     </div>
   )
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './header.css';
+import art from '../../assets/museum.jpeg';
 
 const Header = () => {
   return (
-    <div>Header</div>
+      <div className='blog__header section_padding' id='home'>
+          <div className='blog__header-content'>
+              <h1 className='gradient__text'>Iman Kamran</h1>
+              {/* <p>im just a girl</p> */}
+              {/* <div className='blog__header-content__input'></div> */}
+             
+          </div>
+          {/* <div className='blog__header-image'>
+               <img src={art} alt='art' />
+          </div> */}
+      </div>
   )
 }
 
